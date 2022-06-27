048fb47cc90250f100f433e906034493ae688969
denis5124 <denis5124@tut.by>
2022-06-27 17:52:02 +0300

    aaa

---
+++ JS-begin/Lesson22 30.05 4-JS/script.js
@@ -0,0 +1,6 @@
+function changeColor(){
+    var EL = document.getElementsByTagName('div');
+    EL[0].style.backgroundColor = 'green';
+    EL[0].style.color = 'white';
+    EL[0].style.transition = '5s';
+}
