c7e69e5225c59ba158d92a439fb4499d9f985082
denis5124 <denis5124@tut.by>
2022-07-03 07:50:35 +0300

    aaa

--- HTMLCSS/JS-begin/Lesson22 30.05 4-JS/script.js
+++
@@ -1,6 +0,0 @@
-function changeColor(){
-    var EL = document.getElementsByTagName('div');
-    EL[0].style.backgroundColor = 'green';
-    EL[0].style.color = 'white';
-    EL[0].style.transition = '5s';
-}
--- HTMLCSS/JS-begin/Lesson23 03.06 5-JS/script.js
+++
@@ -1,9 +0,0 @@
-function car(model, color, year, type) {
-    this.model = model;
-    this.color = color;
-    this.year = year;
-    this.type = type
-}
-
-var bmw = new car(320, 'red', 2020);
-var mercedes = new car('GLK', 'black', 2010);
