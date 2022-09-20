dba2a2483a625df55fb8825ca2080fc8faf06c7e
denis5124 <denis5124@tut.by>
2022-09-20 19:46:03 +0300

    aa

---
+++ JavaScript/Classwork/sea battle/seaBattle.js
@@ -0,0 +1,23 @@
+const view = {
+    displayMessage: function (msg) {
+        const messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function (location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    },
+};
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("fgh rty tyu");
