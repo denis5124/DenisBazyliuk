f87fa3ca526e60b5cee5d5ddef90e4ec9a748255
denis5124 <denis5124@tut.by>
2022-09-24 16:07:47 +0300

    sb

--- JavaScript/Classwork/sea battle/seaBattle.js
+++ JavaScript/Classwork/sea battle/seaBattle.js
@@ -25,10 +25,57 @@ const model = {
     shipLength: 3,
     shipsSunk: 0, // кол-во потопленных
     ships: [
-        { locations: ["06", "16", "26"], hits: ["", "", "",] },
-        { locations: ["24", "34", "44"], hits: ["", "", "",] },
-        { locations: ["10", "11", "12"], hits: ["", "", "",] }
-    ],
+        { locations: ["0", "0", "0"], hits: ["", "", "",] },
+        { locations: ["0", "0", "0"], hits: ["", "", "",] },
+        { locations: ["0", "0", "0"], hits: ["", "", "",] }
+        ], 
+
+    generateShipLocations: function () { // метод создаёт корабли, пока массив model.ships не будет заполнен
+        let locations;
+        for (let i = 0; i < this.numShips; i++) { // для каждого корабля генерируем набор позиций
+            do { // цикл do while
+                locations = this.generateShip(); // генерируется новый набор позиций
+            } while (this.collision(locations)); // проверяется, нет ли перекрытий. Если есть перекрытия - ещё одна попытка
+            this.ships[i].locations = locations; // полученные позиции без перекрытия сохраняются в свойстве model.ships.locations
+        }
+    },
+
+    generateShip: function () { // метод создаёт массив со случайными позициями кораблей, не проверяя возможные перекрытия
+        let direction = Math.floor(Math.random() * 2); // генерируется число от 0 до 1 и умножается на 2; затем преобразуется в 0 или 1
+        let row, col;
+        if (direction === 1) { // создаётся горизонтальный корабль
+            row = Math.floor(Math.random() * this.boardSize); // задаётся любая строка
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength)); // задаётся строка с учётом длины корабля
+        } else { // создаётся вертикальный корабль
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+            col = Math.floor(Math.random() * this.boardSize);
+        }
+
+        let newShipLocations = []; // набор позиций нового корабля начинается с пустого массива
+        for (let i = 0; i < this.shipLength; i++) { // в цикле до количества позиций в корабле
+            if (direction === 1) {
+                newShipLocations.push(row + "" + (col + i)); // новая позиция заносится в массив newShipLocations:
+                    // где данные состоят из строки и столбца + i. При первой итерации i=0 и сумма означает начальный столбец
+            } else {
+                newShipLocations.push((row + i) + "" + col); // то же для вертикального
+            }
+        }
+        return newShipLocations; // заполнив массив позициями нового корабля, возвращаем массив методу generateShipLocations
+    },
+
+    // locations - массив позиций нового корабля, который надо разместить на игровом поле
+    collision: function (locations) { // проверка на отсутствие перекрытий
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = model.ships[i]; // для каждого корабля уже находящегося на поле
+            for (var j = 0; j < locations.length; j++) { // проверяем встречаются ли какая-либо из позиций массива locations нового корабля
+                                                        // в массиве locations существующих кораблей
+                if (ship.locations.indexOf(locations[j]) >= 0) { // indexOf проверяет присутствует ли заданная позиция в массиве
+                    return true; // возврат из цикла, функция немедленно завершается и возврщает true
+                }
+            }
+        }
+        return false; // если выполнение дошло до этой точки, значит, ни одна из позиций не была обнаружена в других массивах. Ф-ия возвращает прекрытия отсутствуют
+    },
 
     fire: function (guess) { // метод получает координаты выстрела
 
@@ -126,9 +173,7 @@ function init() {
     fireButton.onclick = handleFireButton; // назначается обработчик события - функция handleFireButton
     let guessInput = document.getElementById("guessInput"); // получение ссылки на поле ввода координат
     guessInput.onkeydown = handleKeyPress; // назначается обработчик события - функция handleKeyPress
+    model.generateShipLocations(); // вызов метода, генерирующего позиции кораблей, кот. заполнит пустые массивы в объекте модели
+            // вызывается из функции init, чобы это проиходило во время загрузки игры (до её начала).
+            // Позиции всех кораблей будут определены к моменту начала
 }
-
-
-
-
-// что бы форма ничего больше не делала
